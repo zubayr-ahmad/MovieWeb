694800fa973a9ef945b62a0fb958304804a4be4b
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-23 20:50:58 +0500

    navbar working

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import './Navbar.css'
 function Navbar() {
+  // var(--primary - color)
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className="main-block">
+      <ul className="menu-bar">
+        <li className="fields" >Home</li>
+        <li className="fields search"><input type="text" className="search-input" placeholder="search movie" /><button id="#searchButton"><i className="fa-solid fa-magnifying-glass fa-beat search-icon" ></i></button></li>
+      <li className="fields">Upcoming</li>
+      <li className="fields">Wish List</li>
+      <li className="fields">Favorites</li>
+      <li className="fields">Login</li>
+      <li className="fields">Register</li>
+    </ul>
+</nav >
   )
 }
 
