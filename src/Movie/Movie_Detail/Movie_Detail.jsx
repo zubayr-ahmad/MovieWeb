95e339d690ea58b2bab8aabf50f44f717b70c9f0
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-24 13:46:32 +0500

    movie detail poster image done

--- src/Movie/Movie_Detail/Movie_Detail.jsx
+++ src/Movie/Movie_Detail/Movie_Detail.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import "./Movie_Detail.css"
 function Movie_Detail() {
     return (
         <div className='container'>
-            <div class="row">
-                <div class="col-8">col-8</div>
-                <div class="col-4">col-4</div>
+            <div className="row ">
+                <div className="col-4 movie_detail__poster_img_container">
+                    <img src='/src/assets/Images/img05.jpg' id='movie_detail__poster_img' alt='image poster'/>
+                    <div id='movie_detail__poster_rating'>7.8</div>
+                </div>
+                <div className="col-1">
+
+                </div>
+                <div className="col-7">col-7</div>
             </div>
         </div>
     )
