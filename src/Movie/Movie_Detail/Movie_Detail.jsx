5241e1f44ffdf2c413a28ffe52500552922cafd1
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-24 14:17:44 +0500

    movie poster rating done

--- src/Movie/Movie_Detail/Movie_Detail.jsx
+++ src/Movie/Movie_Detail/Movie_Detail.jsx
@@ -6,10 +6,21 @@ function Movie_Detail() {
             <div className="row ">
                 <div className="col-4 movie_detail__poster_img_container">
                     <img src='/src/assets/Images/img05.jpg' id='movie_detail__poster_img' alt='image poster'/>
-                    <div id='movie_detail__poster_rating'>7.8</div>
+                    <div id='movie_detail__poster_rating'>
+                        
+                        <div className="icon-star">
+                            <i
+                                className="fa-solid fa-star"
+                                style={{
+                                    color: '#00ff00',
+                                    fontSize: 'x-large'
+                                }}
+                            />
+                        </div>
+                        <p>7.8</p>
+                    </div>
                 </div>
                 <div className="col-1">
-
                 </div>
                 <div className="col-7">col-7</div>
             </div>
