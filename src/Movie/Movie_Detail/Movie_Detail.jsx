212b055eba9f65e91e737b114d8f8f3d80ead578
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-24 19:11:04 +0500

    getting the movie id and adding comments

--- src/Movie/Movie_Detail/Movie_Detail.jsx
+++ src/Movie/Movie_Detail/Movie_Detail.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import "./Movie_Detail.css"
+import { useParams } from 'react-router-dom'
 function Movie_Detail() {
+    const { id } = useParams()
+    console.log(id)
     return (
         <div className='container'>
             <div className="row ">
+                {/* Poster image and rating section (left section) */}
                 <div className="col-4 movie_detail__poster_img_container">
                     <img src='/src/assets/Images/img05.jpg' id='movie_detail__poster_img' alt='image poster'/>
                     <div id='movie_detail__poster_rating'>
@@ -20,8 +24,10 @@ function Movie_Detail() {
                         <p>7.8</p>
                     </div>
                 </div>
-                <div className="col-1">
-                </div>
+                {/* For a gap between the two sections */}
+                <div className="col-1"></div>
+
+                
                 <div className="col-7">col-7</div>
             </div>
         </div>
