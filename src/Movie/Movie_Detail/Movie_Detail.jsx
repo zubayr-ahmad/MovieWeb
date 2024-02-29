97059738cbac6520d141f2dd56329c5e044a7f45
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-02-29 23:13:31 +0500

    heart icon shifting working

--- src/Movie/Movie_Detail/Movie_Detail.jsx
+++ src/Movie/Movie_Detail/Movie_Detail.jsx
@@ -1,17 +1,50 @@
-import React from 'react'
+import { useState } from 'react'
 import "./Movie_Detail.css"
 import { useParams } from 'react-router-dom'
 function Movie_Detail() {
     const { id } = useParams()
     console.log(id)
+    const [showHeart, setShowHeart] = useState(false)
+    const [extra, setExtra] = useState(false)
+    const toggleHeart = () => {
+        setShowHeart((prev) =>{
+            const newPrev = !prev;
+            if(newPrev){
+                document.getElementById('hear2').style.opacity = 1;
+            }
+            else{
+                document.getElementById('hear2').style.opacity = 0;
+            }
+            return newPrev;
+        });
+        // setExtra(true);
+    }
+    console.log(showHeart)
     return (
         <div className='container'>
             <div className="row ">
                 {/* Poster image and rating section (left section) */}
                 <div className="col-4 movie_detail__poster_img_container">
-                    <img src='/src/assets/Images/img05.jpg' id='movie_detail__poster_img' alt='image poster'/>
+                    {/* Heart icons hollow and solid */}
+                    <div className='movie_detail__heart-icons' onClick={toggleHeart}>
+                        <i id="hear1" className="movie_detail_poster_heart_icon fa-regular fa-heart"
+                            style={{
+                                color: '#00ff00',
+                                fontSize: 'xx-large',
+                            }}
+                        ></i>
+                        <i id="hear2" className="movie_detail_poster_heart_icon fa-solid fa-heart"
+                            style={{
+                                color: '#00ff00',
+                                fontSize: 'xx-large',   
+                        }}
+                        ></i>
+
+                    </div>
+                    
+                    {/* Poster image */}
+                    <img src='/src/assets/Images/img05.jpg' id='movie_detail__poster_img' alt='image poster' />
                     <div id='movie_detail__poster_rating'>
-                        
                         <div className="icon-star">
                             <i
                                 className="fa-solid fa-star"
@@ -27,7 +60,7 @@ function Movie_Detail() {
                 {/* For a gap between the two sections */}
                 <div className="col-1"></div>
 
-                
+                {/* Movie details section (right section) */}
                 <div className="col-7">col-7</div>
             </div>
         </div>
