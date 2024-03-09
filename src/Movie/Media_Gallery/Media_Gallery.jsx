2bb0af9c5f47862631839c5a761173213473a878
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-03-09 23:45:25 +0500

    changed structure for further sections of movie

---
+++ src/Movie/Media_Gallery/Media_Gallery.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image_Gallery from './Image_Gallery'
+
+function Media_Gallery() {
+  return (
+    <div className='container'>
+      <Image_Gallery />
+    </div>
+  )
+}
+
+export default Media_Gallery
---
+++ src/Movie/Movie.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Movie_Detail from './Movie_Detail/Movie_Detail'
+import Media_Gallery from './Media_Gallery/Media_Gallery'
+import './Movie.css'
+
+function Movie() {
+  return ( <>
+  <Movie_Detail />
+  <Media_Gallery />
+  </>
+  )
+}
+
+export default Movie
