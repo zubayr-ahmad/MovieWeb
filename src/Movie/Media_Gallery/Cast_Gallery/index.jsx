6985d62c7ff010c5aa1b755fb1b0b6bd413bfdf8
Muhammad Zubair <zubayrahmad0308@gmail.com>
2024-03-23 00:30:34 +0500

    cast styling completed

--- src/Movie/Media_Gallery/Cast_Gallery/index.jsx
+++ src/Movie/Media_Gallery/Cast_Gallery/index.jsx
@@ -6,7 +6,7 @@ const Actor = (props) =>{
     const {name, profile_path, character} = cast;
     // console.log(cast);
     return (<>
-        <img className='cast_gallery__actor_img' src={"https://image.tmdb.org/t/p/w500"+profile_path} alt={cast.name} style={{  }} />
+        <img className='cast_gallery__actor_img' src={profile_path ? "https://image.tmdb.org/t/p/w500" + profile_path : '/src/assets/Images/blank_profile_image.jpg'} alt={cast.name} />
         <div>
             <p className='actor_name' >{name}</p>
             <p className='actor_character'>{character}</p>
@@ -20,13 +20,12 @@ function index() {
     useEffect(() => {
         fetchData('https://api.themoviedb.org/3/movie/550/credits?api_key=<<api_key>>&language=en-US')
             .then(data => setCasts(data.cast));
-        
     }, []);
     // console.log(casts)
     return (
         <div className='container'>
             <div className="row">
-                <h2>Cast</h2>
+                <h1 id="cast_gallery__heading">Cast</h1>
                 <div className="col-6 cast_column1">
                     {casts.slice(0, 10).map((cast, i) => (
                         <div className='cast_gallery__actor' key={i}>    
